12d2dd6e7894f75f26eff23f757abdb9c756f273
Chan Ngo <ngohongchan12a4@gmail.com>
2024-11-24 23:02:05 +0700

    End Application

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
     params: {
@@ -21,16 +22,30 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
         return notFound();
     }
 
+    const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id ); 
+
     return <div>
         <div className="flex m-4 justify-between items-center">
             <h1 className="text-xl font-bold">{snippet.title}</h1>
             <div className="flex gap-4">
                 <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
+               <form action={deleteSnippetAction}>
                 <button className="p-2 border rounded">Delete</button>
+               </form>
             </div>
         </div>
         <pre className="p-3 border rounded bg-gray-200 border-gray-200">
             <code>{snippet.code}</code>
         </pre>
     </div>
+}
+
+export async function generateStaticParams() {
+    const snippets = await db.snippet.findMany();
+
+    return snippets.map((snippet) => {
+        return {
+            id: snippet.id.toString(),
+        }
+    });
 }
--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,28 +1,15 @@
+'use client';
 import React from "react";
 import { db } from "@/db";
 import { redirect } from "next/navigation";
+import * as actions from "@/actions";
+import { useActionState } from "react";
 
 export default function SnippetCreatePage() {
-
-    async function createSnippet(formData: FormData) {
-        'use server';
-
-        const title = formData.get('title') as string;
-        const code = formData.get('code') as string;
-
-        const snippet = await db.snippet.create({
-            data: {
-                title,
-                code
-            }
-        });
-
-        console.log(snippet);
-        redirect('/');
-    }
+   const [formState, action] =  useActionState(actions.createSnippet, { message: '' });
 
     return <div>
-        <form action={createSnippet}>
+        <form action={action}>
             <h3 className="font-bold m-3">Create Snippet</h3>
             <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
@@ -32,6 +19,9 @@ export default function SnippetCreatePage() {
                 <div className="flex gap-4">
                     <label className="w-12" htmlFor="code">Code</label>
                     <textarea name="code" className="border rounded p-2 w-full"></textarea>
+                </div>
+                <div>
+                    {formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null}
                 </div>
                 <button type="submit" className="border rounded p-2 bg-blue-200">
                     create
